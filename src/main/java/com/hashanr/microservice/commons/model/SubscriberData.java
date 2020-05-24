b1f2e90b7bc8864c61680e6c543954b74c5d06c8
HashanR <rmhashan@gmail.com>
2020-05-24 18:01:23 +0530

    Initial domain model project

---
+++ src/main/java/com/hashanr/microservice/commons/model/SubscriberData.java
@@ -0,0 +1,48 @@
+package com.hashanr.microservice.commons.model;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name="subscriber")
+public class SubscriberData {
+    @Id
+    @GeneratedValue
+    private Integer MSISDN;
+    private String name;
+
+    public Integer getMSISDN() {
+        return MSISDN;
+    }
+
+    public void setMSISDN(Integer MSISDN) {
+        this.MSISDN = MSISDN;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public String getConutry() {
+        return conutry;
+    }
+
+    public void setConutry(String conutry) {
+        this.conutry = conutry;
+    }
+
+    private String address;
+    private String conutry;
+
+}
